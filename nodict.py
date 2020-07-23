a8a9489c70e9049054291576b497695b99cd84f2
Keith Garcia <Keith.Garcia444@gmail.com>
2020-07-23 16:42:36 -0400

    final for grading

--- nodict.py
+++ nodict.py
@@ -3,47 +3,57 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = '???'
+__author__ = 'Keith Garcia, Co-Author Ben McKenzie'
 
 
 class Node:
     def __init__(self, key, value=None):
+        """Init Node"""
         self.hash = None
-        self.key = None
-        self.value = None
-        # Your code here
-        return
+        self.key = key
+        self.value = value
+        self.hash = hash(self.key)
 
     def __repr__(self):
-        # Your code here
-        return
+        """Key/Value contents"""
+        return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
-        # Your code here
-        return
+        """Equals"""
+        return self.key == other.key
 
 
 class NoDict:
     def __init__(self, num_buckets=10):
-        self.buckets = None
-        # Your code here
+        """Init NoDict"""
+        self.buckets = [[] for i in range(num_buckets)]
 
     def __repr__(self):
-        # Your code here
-        return
+        """Return a string representing the NoDict contents"""
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i, bucket in enumerate(self.buckets)])
 
     def add(self, key, value):
-        # Your code here
-        return
+        """Put key/value into NoDict"""
+        added_node = Node(key, value)
+        index = added_node.hash % len(self.buckets)
+        for node in self.buckets[index]:
+            if node == added_node:
+                self.buckets[index].remove(node)
+        self.buckets[index].append(added_node)
 
     def get(self, key):
-        # Your code here
-        return
+        """key lookup/return node value"""
+        got_node = Node(key)
+        index = got_node.hash % len(self.buckets)
+        for node in self.buckets[index]:
+            if node == got_node:
+                return node.value
+        raise KeyError(f'{key} not found')
 
     def __getitem__(self, key):
-        # Your code here
-        return
+        """Square bracket reading"""
+        return self.get(key)
 
     def __setitem__(self, key, value):
-        # Your code here
-        return
+        """Square bracket assignment"""
+        self.add(key, value)
